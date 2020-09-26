4bf5d35aceb05f842a5b7cf4d40177c0fa361411
Stanislav <63556577+starovoytov-s@users.noreply.github.com>
2020-09-26 18:09:19 +0300

    Part_01

---
+++ ChatApp/Controllers/ConversationsViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ConversationsViewController.swift
+//  ChatApp
+//
+//  Created by Stanislav Starovoytov on 26.09.2020.
+//
+
+import UIKit
+
+class ConversationsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
--- ChatApp/Controllers/LoginViewController.swift
+++ ChatApp/Controllers/LoginViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  LoginViewController.swift
 //  ChatApp
 //
 //  Created by Stanislav Starovoytov on 26.09.2020.
@@ -7,13 +7,14 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class LoginViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+
         // Do any additional setup after loading the view.
     }
+    
 
 
 }
-
