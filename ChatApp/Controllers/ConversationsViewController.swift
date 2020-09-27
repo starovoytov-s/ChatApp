dccf653c9dfd0bfaf49ed7117dbdc458e32f7e16
Stanislav <63556577+starovoytov-s@users.noreply.github.com>
2020-09-27 14:17:19 +0300

    Part_02

--- ChatApp/Controllers/ConversationsViewController.swift
+++ ChatApp/Controllers/ConversationsViewController.swift
@@ -11,8 +11,28 @@ class ConversationsViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        view.backgroundColor = .red
+        
     }
+    
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        let isLoggedIn = UserDefaults.standard.bool(forKey: "Logged_in")
+        
+        if !isLoggedIn {
+            let vc = LoginViewController()
+            let nav = UINavigationController(rootViewController: vc)
+            nav.modalPresentationStyle = .fullScreen
+            present(nav, animated: true)
+            
+        }
+    }
+    
+    
+    
 
 
 }
---
+++ ChatApp/Resources/Extensions.swift
@@ -0,0 +1,40 @@
+//
+//  Extensions.swift
+//  ChatApp
+//
+//  Created by Stanislav Starovoytov on 27.09.2020.
+//
+
+import Foundation
+import UIKit
+
+
+extension UIView {
+    
+    public var width: CGFloat {
+        return self.frame.size.width
+    }
+    
+    public var height: CGFloat {
+        return self.frame.size.height
+    }
+    
+    public var top: CGFloat {
+        return self.frame.origin.y
+    }
+    
+    public var bottom: CGFloat {
+        return self.frame.size.height + self.frame.origin.y
+    }
+    
+    public var left: CGFloat {
+        return self.frame.origin.x
+    }
+    
+    public var right: CGFloat {
+        return self.frame.size.width + self.frame.origin.x
+    }
+    
+    
+}
+
