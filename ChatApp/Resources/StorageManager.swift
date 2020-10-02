fdf131c3325121911731852dbfb42a9a3dc37de2
Stanislav <63556577+starovoytov-s@users.noreply.github.com>
2020-10-02 13:34:47 +0300

    Part_14

--- ChatApp/Resources/StorageManager.swift
+++ ChatApp/Resources/StorageManager.swift
@@ -42,6 +42,32 @@ final class StorageManager {
     }
     
     
+    ///upload iamge that will be sent in a conversation
+    public func uploadMessagePhoto(with data: Data, fileName: String, completion: @escaping UploadPictureComletion ) {
+        storage.child("message_images/\(fileName)").putData(data, metadata: nil) { (metadata, error) in
+            guard error == nil else {
+                //fail
+                print("failed to upload data to firebase for picture")
+                completion(.failure(StorageErrors.failedToUpload))
+                return
+            }
+            self.storage.child("message_images/\(fileName)").downloadURL { (url, error) in
+                guard let url = url else {
+                    print("Failed to get download URL")
+                    completion(.failure(StorageErrors.failedToGetDownloadURL))
+                    return
+                }
+                let urlString = url.absoluteString
+                print("download url returned: \(urlString)")
+                completion(.success(urlString))
+                
+            }
+            
+        }
+        
+    }
+    
+    
     public enum StorageErrors: Error {
         case failedToUpload
         case failedToGetDownloadURL
